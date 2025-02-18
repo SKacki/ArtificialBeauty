751783e86affc5e374c34c1262dd914e79d9f219
Szymon <szymon.kacki@nfosigw.gov.pl>
2025-02-18 15:34:30 +0100

    Initial commmit

---
+++ DAL/Contexts/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Obiegi.DAL.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        //public DbSet<Uzytkownik> Uprawnienia { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Ocena>()
+            //    .HasOne(o => o.ZatwiedzajacyOcene)
+            //    .WithMany(z => z.ZatwierdzoneOceny)
+            //    .HasForeignKey(o => o.ZatwierdzajacyOceneId)
+            //    .IsRequired(false);
+        }
+    }
+}
---
+++ WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    //public class HomeForecastController(ILogger<HomeForecastController> logger) : ControllerBase
+    public class HomeController() : ControllerBase
+    {
+        //private readonly ILogger<HomeForecastController> _logger = logger;
+
+        [HttpGet("test")]
+        public async Task<string> Get()
+        {
+            return "Hello World";
+        }
+    }
+}
